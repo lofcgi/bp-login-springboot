d6525a3fa56062450ea8d9da49d9e790a7183df9
lofcgo1205@gmail.com <lofcgo1205@gmail.com>
2025-04-22 11:03:08 +0900

    [Setting] 프로젝트 초기화

---
+++ src/main/java/com/boilerplate/login_springboot/LoginSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.boilerplate.login_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boilerplate/login_springboot/LoginSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerplate.login_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
